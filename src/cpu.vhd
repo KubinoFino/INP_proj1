0ab4f59a3d44f812022c86ec62e5a53152f4dd57
Jakub Kačka <jakub.kacka4@gmail.com>
2023-11-09 18:41:21 +0100

    added state handeling but not working

--- src/cpu.vhd
+++ src/cpu.vhd
@@ -46,52 +46,55 @@ end cpu;
 -- ----------------------------------------------------------------------------
 architecture behavioral of cpu is
 
--- PC (ukazatel do pamate programu)
-signal PC_addr : std_logic_vector(12 downto 0);
-signal PC_inc : std_logic;
-signal PC_dec : std_logic;
+  -- PC (ukazatel do pamate programu)
+  signal PC_addr : std_logic_vector(12 downto 0);
+  signal PC_inc : std_logic;
+  signal PC_dec : std_logic;
 
--- PTR (ukazatel do pamate dat)
-signal PTR_addr : std_logic_vector(12 downto 0);
-signal PTR_inc : std_logic;
-signal PTR_dec : std_logic;
+  -- PTR (ukazatel do pamate dat)
+  signal PTR_addr : std_logic_vector(12 downto 0);
+  signal PTR_inc : std_logic;
+  signal PTR_dec : std_logic;
 
---CNT (while counter)
-signal CNT_data : std_logic_vector(12 downto 0);
-signal CNT_inc : std_logic;
-signal CNT_dec : std_logic;
-signal CNT_one : std_logic;
+  --CNT (while counter)
+  signal CNT_data : std_logic_vector(12 downto 0);
+  signal CNT_inc : std_logic;
+  signal CNT_dec : std_logic;
+  signal CNT_one : std_logic;
 
--- MX1
-signal MX1 : std_logic_vector(0 downto 0) := "0";
--- 0 adresa programu
--- 1 adresa dat
+  -- MX1
+  signal MX1 : std_logic_vector(0 downto 0) := "0";
+  -- 0 adresa programu
+  -- 1 adresa dat
 
--- MX2
-signal MX2 : std_logic_vector(1 downto 0) := "00";
--- 00 hodnota zo vstupu
--- 01 hodnota z aktualnej bunky +1
--- 10 hodnota z aktualnej bunky -1
+  -- MX2
+  signal MX2 : std_logic_vector(1 downto 0) := "00";
+  -- 00 hodnota zo vstupu
+  -- 01 hodnota z aktualnej bunky +1
+  -- 10 hodnota z aktualnej bunky -1
+  -- 11 posledna citana hodnota z pamate
 
--- FSM
-type fsm_states is (
-  start,
-  fetch,
-  decode,
-  increment_ptr,
-  decrement_ptr,
-  increment_val,
-  decrement_val,
-  while_start,
-  while_end,
-  break,
-  put_char,
-  get_char,
-  sreturn,
-  sothers
-);
-signal state : fsm_states := start;
-signal next_state : fsm_states := start;
+  -- FSM
+  type fsm_states is (
+    start,
+    fetch,
+    decode,
+    increment_ptr,
+    decrement_ptr,
+    increment_val,
+    end_increment_val,
+    decrement_val,
+    end_decrement_val,
+    while_start,
+    while_end,
+    break,
+    put_char,
+    get_char,
+    sreturn,
+    sothers
+  );
+  signal state : fsm_states := start;
+  signal next_state : fsm_states := start;
 
 begin
 
@@ -105,7 +108,7 @@ begin
   begin
         if (RESET = '1') then
           PC_addr <= (others => '0');
-        elseif (CLK'event and CLK = '1') then
+        elsif (CLK'event) and (CLK = '1') then
           if (PC_inc = '1') then
             PC_addr <= PC_addr + 1;
           elsif (PC_dec = '1') then
@@ -117,8 +120,8 @@ begin
   ptr: process(CLK, RESET)
   begin 
         if (RESET = '1') then
-          PTR_addr <= (others => '0');
-        elsif (CLK'event and CLK = '1') then
+          PTR_addr <= "1000000000000";
+        elsif (CLK'event) and (CLK = '1') then
           if (PTR_inc = '1') then
             PTR_addr <= PTR_addr + 1;
           elsif (PTR_dec = '1') then
@@ -131,13 +134,13 @@ begin
   begin
         if (RESET = '1') then
           CNT_data <= (others => '0');
-        elsif (CLK'event and CLK = '1') then
+        elsif (CLK'event) and (CLK = '1') then
           if (CNT_inc = '1') then
             CNT_data <= CNT_data + 1;
           elsif (CNT_dec = '1') then
             CNT_data <= CNT_data - 1;
           elsif (CNT_one = '1') then
-            CNT_data <= "000000000001";
+            CNT_data <= "0000000000001";
           end if;
         end if;
   end process;
@@ -146,7 +149,8 @@ begin
 
   DATA_WDATA <= IN_DATA when MX2 = "00" else
                 DATA_RDATA + 1 when MX2 = "01" else
-                DATA_RDATA - 1;
+                DATA_RDATA - 1 when MX2 = "10" else
+                DATA_RDATA when MX2 = "11";
 
   fsm: process(CLK, RESET)
   begin
@@ -157,14 +161,16 @@ begin
         end if;
   end process;
 
-  fsm_next: process(state, DATA_RDATA, IN_VLD, OUT_BUSY)
+  fsm_next: process(state, EN, DATA_RDATA, IN_VLD, OUT_BUSY, DATA_RDATA)
   begin 
           PC_inc <= '0';
           PC_dec <= '0';
 
           PTR_inc <= '0';
           PTR_dec <= '0';
-
+          READY <= '1';
+          DONE <= '0'; 
+          
           CNT_inc <= '0';
           CNT_dec <= '0';
           CNT_one <= '0';
@@ -208,6 +214,105 @@ begin
                 when others =>
                     next_state <= sothers;
             end case;
+            
+            when increment_ptr =>
+                ptr_inc <= '1';
+                pc_inc <= '1';
+                next_state <= fetch;
+
+            when decrement_ptr =>
+                ptr_dec <= '1';
+                pc_inc <= '1';
+                next_state <= fetch;
+
+            when increment_val =>
+                DATA_EN <= '1';
+                DATA_RDWR <= '1';
+                MX1 <= "1";
+                next_state <= end_increment_val;
+
+            when end_increment_val =>
+                MX2 <= "01";
+                DATA_EN <= '1';
+                DATA_RDWR <= '1';
+                pc_inc <= '1';
+                next_state <= fetch;
+
+            when decrement_val =>
+                DATA_EN <= '1';
+                DATA_RDWR <= '1';
+                MX1 <= "1";
+                next_state <= end_decrement_val;
+
+            when end_decrement_val =>
+                MX2 <= "10";
+                DATA_EN <= '1';
+                DATA_RDWR <= '1';
+                pc_inc <= '1';
+                next_state <= fetch;
+
+            when while_start =>
+                data_en <= '1';
+                data_rdwr <= '0';
+                mx1 <= "1";
+                mx2 <= "00";
+                if (DATA_RDATA = "000000000000") then
+                  next_state <= while_end;
+                else
+                  next_state <= fetch;
+                end if;
+
+            when while_end =>
+                data_en <= '1';
+                data_rdwr <= '0';
+                mx1 <= "1";
+                mx2 <= "00";
+                if (DATA_RDATA = "000000000000") then
+                  next_state <= fetch;
+                else
+                  next_state <= while_start;
+                end if;
+
+            when break =>
+                data_en <= '1';
+                data_rdwr <= '0';
+                mx1 <= "1";
+                mx2 <= "00";
+                next_state <= fetch;
+
+            when put_char =>
+                out_we <= '1';
+                out_data <= DATA_RDATA;
+                if (OUT_BUSY = '0') then
+                  next_state <= fetch;
+                else
+                  next_state <= put_char;
+                end if;
+
+            when get_char =>
+                in_req <= '1';
+                if (IN_VLD = '1') then
+                  data_en <= '1';
+                  data_rdwr <= '1';
+                  mx1 <= "1";
+                  mx2 <= "00";
+                  next_state <= fetch;
+                else
+                  next_state <= get_char;
+                end if;
+
+            when sreturn =>
+                pc_dec <= '1';
+                next_state <= fetch;
+
+            when sothers =>
+                next_state <= fetch;
+
+            when others =>
+                next_state <= fetch;
+          end case;
+  end process;
+
 
 end behavioral;
 
